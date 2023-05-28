70ede562fac8b070d3b9e9517a2dfcc91c9a000f
jollysinao <jollysinao@gmail.com>
2023-05-28 10:20:18 -0700

    printf

--- handle_print.c
+++ handle_print.c
@@ -18,7 +18,7 @@ int i, unknow_len = 0, printed_chars = -1;
 fmt_t fmt_types[] = {
 {'c', print_char}, {'s', print_string}, {'%', print_percent},
 {'i', print_int}, {'d', print_int}, {'b', print_binary},
-{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
+{'u', print_unsigned}, {'o', print_octal}, {'X', print_hexadecimal},
 {'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
 {'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 };
