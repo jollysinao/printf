ef96fa241462154350a25390ab9279e3a18af3dc
Joyce Stock <129006220+Stocky11@users.noreply.github.com>
2023-05-30 11:02:06 -0700

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -52,7 +52,7 @@ return (printed_chars);
 /**
 * print_buffer - Prints the contents of the buffer if it exist
 * @buffer: Array of chars
-* @buff_ind: Index at which to add next character, represents the length.
+* @buff_ind: Index at which to add next char, represents the length.
 */
 void print_buffer(char buffer[], int *buff_ind)
 {
