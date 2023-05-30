4c237849fefe966cdab071e8f674fed3db28c53f
Joyce Stock <129006220+Stocky11@users.noreply.github.com>
2023-05-30 10:23:30 -0700

    Update functions.c

--- functions.c
+++ functions.c
@@ -150,7 +150,7 @@ return (write_number(is_negative, i, buffer, flags, width, precision, size));
 * @width: get width.
 * @precision: Precision specification
 * @size: Size specifier
-* Return: Numbers of char printed.
+* Return: Numb of char printed.
 */
 int print_binary(va_list types, char buffer[],
 int flags, int width, int precision, int size)
